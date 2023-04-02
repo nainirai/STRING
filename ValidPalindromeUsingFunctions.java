17263021a1fdff606a50fbc460c3bed648dd6223
Naini Rai <128786091+nainirai@users.noreply.github.com>
2023-04-02 09:15:09 +0530

    Delete ValidPalindromeUsingFunctions.java

--- ValidPalindromeUsingFunctions.java
+++
@@ -1,7 +0,0 @@
-public boolean isPalindrome(String s) {
-         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-          // compare with the reverse of the string
-        StringBuilder sb = new StringBuilder(s);
-        return s.equals(sb.reverse().toString());
-        }
-        
